56dba94f5623cb6d97035726d56e630a41a93c88
bergpb <lindembergdepaulo@gmail.com>
2018-12-09 11:58:32 -0300

    temperatura adicionado ao dashboard

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -25,6 +25,13 @@ def setPin(pin_number):
     except:
         pass
 
+# only for test
+# def checkPin(pin):
+#     return True
+    
+# def setPin(pin):
+#     return True
+
 def statusInfo():
     process = subprocess.getstatusoutput('ps -aux | wc -l')[1]
     uptime = subprocess.getstatusoutput('uptime -p')[1]
@@ -34,11 +41,13 @@ def statusInfo():
     sdcard_free = subprocess.getstatusoutput('df -h | grep \'/dev\'| cut -c 30-31 | head -1')[1]
     sdcard_percent = subprocess.getstatusoutput('df -h | grep \'/dev\'| cut -c 35-36 | head -1')[1]
     local_ip = subprocess.getstatusoutput('ifconfig wlan0 |  grep inet | cut -c 14-26 | head -1')[1]
+    cpu_temp = float(subprocess.getstatusoutput('cat /sys/class/thermal/thermal_zone0/temp')[1]) / 1000
     rx_float_mb = round(float(subprocess.getstatusoutput('cat /sys/class/net/wlan0/statistics/rx_bytes')[1]) / 1024 / 1024, 2)
     tx_float_mb = round(float(subprocess.getstatusoutput('cat /sys/class/net/wlan0/statistics/tx_bytes')[1]) / 1024 / 1024, 2)
     return{
         'process': process,
         'uptime': uptime,
+        'cpu_temp': cpu_temp,
         'mem_used': mem_used,
         'mem_free': mem_free,
         'sdcard_used': sdcard_used,
