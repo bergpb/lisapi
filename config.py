9a4846e844f6d10f52b551780a7274207a3216c4
bergpb <lindembergdepaulo@gmail.com>
2019-03-06 11:33:46 -0300

    melhorias nas configurações do projeto

--- config.py
+++ config.py
@@ -5,15 +5,17 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    # to generate a secret key
+    # python -c 'import os; os.urandom(24)'
     SECRET_KEY = 'your_secret_key'
 
 class production(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
-    URL_API = 'https://receding-cat-9495.dataplicity.io/api/status'
+    URL_API = os.getenv('LISA_API')
 
 class development(Config):
     DEBUG = True
     DEVELOPMENT = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    URL_API = 'http://0.0.0.0:5000/api/status'
+    URL_API = os.getenv('LISA_API_DEV')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage_dev.db')
