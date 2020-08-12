dc1cc33eedea8963df92abc497f62ebd88dc399c
bergpb <lindembergdepaulo@gmail.com>
2020-08-11 21:47:24 -0300

    flask_sslify removed

--- lisapi/__init__.py
+++ lisapi/__init__.py
@@ -1,7 +1,6 @@
 import os
 import config
 from flask import Flask
-from flask_sslify import SSLify
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
